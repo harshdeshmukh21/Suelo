d07065bbff5fa724c42545d79bdf4bffc09c221f
Arya <128399498+aryas006@users.noreply.github.com>
2024-04-05 23:10:59 +0530

    initial commit
    initialising frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/ui/button'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-black h-screen'>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
