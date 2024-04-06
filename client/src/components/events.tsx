598c98f6a09eeab7eabff33c199105fa9379fec2
harshdeshmukh21 <deshmukhharsh2122@ternaengg.ac.in>
2024-04-06 22:04:52 +0530

    events

--- client/src/components/events.tsx
+++ client/src/components/events.tsx
@@ -1,15 +1,87 @@
-import Sidebar from "@/comps/Side-bar";
+import Sidebar from "../comps/Side-bar.tsx";
+import chipko from "../assets/image.png";
+import { Card, CardContent } from "@/components/card.tsx";
 import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const JoinAct = () => {
+  return (
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  );
+};
 
 const Events = () => {
   return (
-    <div className="flex row">
-      <Sidebar></Sidebar>
-      <div className="h-[150px] w-[1500px] mt-20 bg-[url('https://cdn.sanity.io/images/oyzyxja8/v2/062f3d6cc3c04944e38c95589f9cb7b3061582c2-2500x1080.jpg?w=2048&q=90&auto=format')]"></div>
+    <div className="flex">
+      <Sidebar />
+      <div
+        className="h-[250px] flex bg-contain border-black border-8 rounded-md w-full m-2 relative"
+        style={{
+          backgroundImage: `url(${chipko})`,
+        }}
+      >
+        <h1 className="text-4xl font-semi-bold m-4 text-white">Events</h1>
+        <div className="flex justify-end items-end w-full m-4">
+          <button
+            className="mt-16 bg-[#1F2114] text-white p-3 rounded-xl"
+            onClick={JoinAct}
+          >
+            Create Activity
+          </button>
+        </div>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-center gap-16">
+          <p className="text-black font-semi-bold text-4xl mt-[600px]">
+            Join an Activity
+          </p>
+          <div className="flex flex-col items-center justify-center h-full">
+            <Carousel
+              opts={{
+                align: "start",
+              }}
+              orientation="vertical"
+              className="w-full max-w-xs"
+            >
+              <CarouselContent className="-mt-1 h-[200px]">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <CarouselItem key={index} className="pt-1 md:basis-1/2">
+                    <div className="p-1">
+                      <Card className="w-[300px] bg-[#1F2114] text-white">
+                        <CardContent className="flex items-center justify-center p-6">
+                          <span className="text-4xl font-semi-bold">
+                            {index + 1}
+                          </span>
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ client/src/components/form.tsx
@@ -0,0 +1,43 @@
+import LoginButton from "./login";
+
+const Form = () => {
+  return (
+    <>
+      <div className="relative flex flex-col items-center justify-center h-full">
+        <div className="flex flex-col justify-center items-center h-[425px] w-[450px] bg-black rounded-[20px]">
+          <div>
+            <div className="font-bold text-3xl text-white font-montserrat mt-4 ml-4 mb-10">
+              Get Started
+            </div>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            className="font-montserrat rounded bg-white text-black p-4 outline-none mt-4 mb-1 h-[50px] w-[300px]"
+          />
+          <input
+            type="email"
+            placeholder="Email id"
+            className="font-montserrat rounded bg-white text-black p-4 outline-none mb-1 h-[50px] w-[300px]"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="font-montserrat rounded bg-white text-black p-4 outline-none mb-1 h-[50px] w-[300px]"
+          />
+          <button className="font-montserrat bg-[#1F2114] text-white p-5 rounded mt-6 text-center h-[20px] flex flex-col justify-center">
+            Sign Up
+          </button>
+        </div>
+        <div className="flex flex-row gap-2">
+          <div className="text-black mt-4 font-montserrat opacity-60 text-xs">
+            Already have an account?
+          </div>
+          <LoginButton></LoginButton>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Form;
